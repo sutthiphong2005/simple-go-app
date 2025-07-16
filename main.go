41b39d64ded08c4404f79a02052acd2151095010
sutthiphong2005 <sutthiphong2005@gmail.com>
2025-07-16 20:46:57 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
     fmt.Println("Hello, GitHub from Go3324 cc !")
+
+
+    var unusedVar int // unused variable
+
+    file, _ := os.Open("nonexistent.txt") // error is ignored
+
+    fmt.Println("Hello, Linter!") // no newline at end of file
+
+    
 }
